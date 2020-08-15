11a447a658ead947053b9963fdd5cef3fc151cf5
Mauro Ali Gomez <maurogo@lagash.com>
2020-08-15 17:30:30 -0300

    First commit

---
+++ WC.Context/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WC.Context
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WC.Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WC.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
